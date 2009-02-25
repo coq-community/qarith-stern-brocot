4163d0239e360af81bc64d0d5ff4ec7e53e601af
milad <milad@0cf17b13-060f-0410-b1b1-c666bec9822a>
2009-02-25 15:22:00 +0000

    fix the license URL. (last attempt!)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@932 0cf17b13-060f-0410-b1b1-c666bec9822a

--- Qmax_min.v
+++ Qmax_min.v
@@ -3,7 +3,7 @@
 (* This file is distributed under the terms of the                      *)
 (* GNU Lesser General Public License Version 2.1                        *)
 (* A copy of the license can be found at                                *)
-(*                  <http://www.gnu.org/licenses/lgpl-2.1.thml>               *)
+(*                  <http://www.gnu.org/licenses/lgpl-2.1.html>         *)
 (************************************************************************)
 
 Require Export Qsyntax.
